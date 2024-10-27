b4ca74fb87092a7a2a97d5fa5cbbfa0f34508db7
Prakhar <prakharchandra1803@gmail.com>
2024-10-27 17:00:19 +0530

    Update task.py

--- task.py
+++ task.py
@@ -2,13 +2,12 @@ import time
 import subprocess
 
 def run_task():
-    # Simulate a task 
+
     print("Running task...")
-    time.sleep(5)  # Adjust
+    time.sleep(5)  
     print("Task completed.")
 
 def notify_whatsapp_bot():
-    # Call the js script
     subprocess.run(["node", "sendVideo.js"])
 
 if __name__ == "__main__":
