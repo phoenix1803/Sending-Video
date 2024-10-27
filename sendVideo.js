e3f4962689582d108a7f578b65ba8c11dac0380a
Prakhar <prakharchandra1803@gmail.com>
2024-10-27 17:00:51 +0530

    Update sendVideo.js

--- sendVideo.js
+++ sendVideo.js
@@ -2,7 +2,6 @@ const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const path = require('path');
 
-// Init
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
@@ -17,17 +16,15 @@ client.on('qr', (qr) => {
 
 client.on('ready', async () => {
     console.log('Client is ready!');
-
-    // Specify the phone number 
+ 
     const recipientNumber = "919876543210@s.whatsapp.net";  
 
     const videoPath = path.join(__dirname, 'video.mp4');
     
     try {
-        // Load 
+        
         const media = MessageMedia.fromFilePath(videoPath);
 
-        // Send video 
         await client.sendMessage(recipientNumber, media);
         console.log('Video sent successfully!');
     } catch (error) {
